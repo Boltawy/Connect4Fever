3e4f30e545c7f52d80e6727fa6a08c8428082e8a
Bolt4Action <Zeyadisma3eel@gmail.com>
2025-09-20 22:37:54 +0300

    another try

--- src/app/services/multiplayer.service.ts
+++ src/app/services/multiplayer.service.ts
@@ -12,7 +12,7 @@ export class MultiplayerService {
   socket: Socket;
 
   constructor() {
-    this.socket = io('http://c4fever.eu-4.evennode.com', {
+    this.socket = io('https://c4fever.eu-4.evennode.com', {
       transports: ['websocket'],
       secure: true,
     });
